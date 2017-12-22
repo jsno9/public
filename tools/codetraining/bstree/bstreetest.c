7137c9fd96a3d7cd9d4d24598f9fc8c9a16e6cd8
jason jiang <jsno9@163.com>
2017-12-22 21:25:30 +0800

    modified:   bstreetest.c

--- tools/codetraining/bstree/bstreetest.c
+++ tools/codetraining/bstree/bstreetest.c
@@ -99,7 +99,7 @@ Node *bstree_prenode(BSTree z)
 	
 	Node *y=(Node *)malloc(sizeof(Node));
 	y=z->parent;
-	while(z!=y>right && z->parent!=NULL)
+	while(z!=y->right && z->parent!=NULL)
 	{
 		z=y;
 		y=y->parent;
@@ -118,7 +118,7 @@ Node *bstree_successornode(BSTree z)
 	
 	Node *y=(Node *)malloc(sizeof(Node));
 	y=z->parent;
-	while(z!=y>left && z->parent!=NULL)
+	while(z!=y->left && z->parent!=NULL)
 	{
 		z=y;
 		y=y->parent;
@@ -130,11 +130,13 @@ Node *bstree_successornode(BSTree z)
 Node *bstree_insertnode(BSTree tree,Node *z)
 {
 	Node *x=tree;
+    Node *y=NULL;
 	if(tree==NULL)
 		return z;
-	
+	//printf("insert 1\n");
 	while(x!=NULL)
 	{
+        y=x;
 		if(x->key>z->key)
 		{
 			x=x->left;
@@ -144,8 +146,8 @@ Node *bstree_insertnode(BSTree tree,Node *z)
 			x=x->right;
 		}
 	}
-	
-	x=x->parent;
+	//printf("insert y->key=%d\n",y->key);
+	x=y;
 	if(x->key>z->key)
 	{
 		x->left=z;
@@ -156,9 +158,142 @@ Node *bstree_insertnode(BSTree tree,Node *z)
 		x->right=z;
 		z->parent=x;
 	}
-	
+	//printf("insert 3\n");
 	return tree;
 	
+}
+
+Node *insert_key(BSTree tree,int key)
+{
+    Node *z=create_node(key,NULL,NULL,NULL);
+    
+    if(z!=NULL)
+    {
+        printf("z->key=%d\n",z->key);
+        bstree_insertnode(tree,z);
+    }
+    else
+        return tree;
+}
+
+Node *treenode_delete(BSTree tree,Node *z)
+{
+    Node *now=NULL;
+    Node *son=NULL;
+    if(z->left==NULL || z->right==NULL)
+    {
+        now=z;
+    }
+    else
+    {
+        now=bstree_successornode(z);
+    }
+
+    if(now->left!=NULL)
+        son=now->left;
+    else
+        son=now->right;
+
+    if(son!=NULL)
+        son->parent=now->parent;
+
+    if(son->parent==NULL)
+        tree=now;
+    else if(now==son->parent->left)
+        son->parent->left=son;
+    else
+        son->parent->right=son;
+
+    if(son!=z)
+        z->key=son->key;
+
+    if(now!=NULL)
+        free(now);
+
+    return tree;
+    
+    
+}
+
+
+
+Node *delete_key(BSTree tree,int key)
+{
+    Node *z=bstree_search(tree,key);
+    if(z!=NULL)
+        treenode_delete(tree,z);
+
+    return tree;
+}
+
+
+void print_bstree(BSTree tree,int key,int direction)
+{
+    if(tree!=NULL)
+    {
+        if(direction==0)
+            printf("%2d is root\n", tree->key);
+        else
+            printf("%2d is %2d's %6s child\n",tree->key,key,direction==1?"right" : "left");
+        
+        print_bstree(tree->left, tree->key, -1);
+        print_bstree(tree->right,tree->key,  1);
+    }
+    
+}
+
+void destroy_bstree(BSTree tree)
+{
+    if(tree==NULL)
+        return;
+    
+    if(tree->left!=NULL)
+        destroy_bstree(tree->left);
+    if(tree->right!=NULL)
+        destroy_bstree(tree->right);
+    
+    free(tree);
+}
+
+
+void main()
+{
+    static int arr[]= {1,5,4,3,2,6};
+    int i, ilen;
+    BSTree root=NULL;
+
+    printf("== 依次添加: ");
+    ilen =  (sizeof(arr)) / (sizeof(arr[0]));
+    for(i=0; i<ilen; i++)
+    {
+        printf("%d ", arr[i]);
+        root = insert_key(root, arr[i]);
+        //print_bstree(root, root->key, 0);
+    }
+    printf("\n== 前序遍历: ");
+    preorder(root);
+
+    printf("\n== 中序遍历: ");
+    inorder(root);
+
+    printf("\n== 后序遍历: ");
+    postorder(root);
+    printf("\n");
+
+    printf("== 最小值: %d\n", bstree_minimum(root)->key);
+    printf("== 最大值: %d\n", bstree_maximum(root)->key);
+    printf("== 树的详细信息: \n");
+    print_bstree(root, root->key, 0);
+
+    printf("\n== 删除根节点: %d", arr[3]);
+    root = delete_key(root, 7);
+
+    printf("\n== 中序遍历: ");
+    inorder(root);
+    printf("\n");
+
+    // 销毁二叉树
+    destroy_bstree(root);
 }
 
 
