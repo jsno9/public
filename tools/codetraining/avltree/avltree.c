301e1b897ca2e4a9ffc91bef67777b32552d3088
jsno9 <jsno9@163.com>
2018-01-01 19:42:15 +0800

    avltree

--- tools/codetraining/avltree/avltree.c
+++ tools/codetraining/avltree/avltree.c
@@ -6,7 +6,7 @@ typedef struct node{
     struct node *left;
     struct node *right;
     int height
-}Node;
+}Node,Tree;
 
 static Node *creat_node(int key,Node *left,Node *right)
 {
@@ -49,4 +49,123 @@ static Node *left_left_rotation(Node *p)
     
     return pleft;
 }
+
+static Node *right_right_rotation(Node *p)
+{
+    Node *pright;
+    
+    pright=p->right;
+    p->right=pright->left;
+    pright->left=p;
+    
+    p->height=1+max(heighttree(p->left),heighttree(p->right));
+    pright->height=1+max(heighttree(pright->right),heighttree(pright->left));
+    
+    return pright;
+}
+
+static Node *left_right_rotation(Node *p)
+{
+	p->left=right_right_rotation(p->left);
+
+	return left_left_rotation(p);
+}
+
+static Node *right_left_rotation(Node *p)
+{
+	p->left=left_left_rotation(p->left);
+
+	return right_right_rotation(p);
+}
+
+Node *insert_node(Node *tree,int key)
+{
+	if(tree==NULL)
+	{
+        tree = creat_node(key,NULL,NULL);
+	}
+    else if(key<tree->key)
+    {
+        tree->left = insert_node(tree->left,key);
+        if(heighttree(tree->left)-heighttree(tree->right) == 2)
+        {
+            if(key<tree->left->key)
+                tree=left_left_rotation(tree);
+            else
+                tree=left_right_rotation(tree);
+        }
+    }
+    else if(key>tree->key)
+    {
+        tree->right = insert_node(tree->right,key);
+        if(heighttree(tree->right)-heighttree(tree->left) == 2)
+        {
+            if(key>tree->right->key)
+                tree=right_right_rotation(tree);
+            else
+                tree=right_left_rotation(tree);
+        }
+    }
+    else
+    {
+        printf("the key was in the tree already\n");
+    }
+
+    tree->height=1+max(heighttree(tree->right),heighttree(tree->left));
+	
+
+}
+
+Node *delete_node(Node *tree,Node *p)
+{
+    if(tree==NULL || p==NULL)
+        return NULL;
+
+    if(p->key<tree->key)
+    {
+        tree->left = delete_node(tree->left,p);     
+        if(heighttree(tree->right)-heighttree(tree->left) == 2)
+        {
+            if(key>tree->right->key)
+                tree=right_right_rotation(tree);
+            else
+                tree=right_left_rotation(tree);
+        } 
+    }
+    else if(p->key>tree->key)
+    {
+        tree->right = delete_node(tree->right,p);
+        if(heighttree(tree->left)-heighttree(tree->right) == 2)
+        {
+            if(key<tree->left->key)
+                tree=left_left_rotation(tree);
+            else
+                tree=left_right_rotation(tree);
+        }
+    }
+    else
+    {
+        if((tree->left != NULL) && (tree->right != NULL))
+        {
+            if(heighttree(tree->left)>heighttree(tree->right))
+            //Node *max=
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
